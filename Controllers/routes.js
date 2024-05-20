df7555e7d81e74a7e742469824943a9b01fa1ea7
beatrizfs04 <beatrizfs2004@gmail.com>
2024-05-20 15:19:54 +0100

    router.js e funções de users.js

--- Controllers/routes.js
+++ Controllers/routes.js
@@ -4,29 +4,53 @@ const routes = express.Router();
 const SQL = require('../Controllers/sql');
 const Users = require('../Controllers/users');
 
-/* routes.get('/', async (req, res) => {
-    const gotData = await SQL.find({});
-    console.log(gotData);
-});*/
-
+// Ver todos os utilizadores
 routes.get('/users', async (req, res) => {
     const gotUsers = Users.checkUsers();
-    res.json(gotUsers);
+    res.status(200).send(gotUsers);
+    res.status(400).send(`Não foi possivel aceder à informação da UsersDB.`);
 })
 
+// Ver um utilizador através do username
+routes.get('/users', async (req, res) => {
+    const username = req.body;
+    const gotUser = Users.checkUser(username);
+    res.status(200).send(gotUser);
+    res.status(400).send(`Não foi possivel encontrar o utilizador com o username: ${username}.`);
+})
+
+// Criar um utilizador
 routes.post('/users', async (req, res) => {
-    const gotUsers = Users.createUsers();
-    res.json(gotUsers);
+    const {username, email, password, phone} = req.body;
+    const newUser = {username: username, email: email, password: password, phone: phone};
+    const createdUser = Users.createUser(newUser);
+    res.status(200).send(createdUser);
+    res.status(400).send(`Não foi possivel acrescentar o utilizador com o username: ${username}, o email: ${email}, a password: ${password}, o phone: ${phone}.`);
 })
 
+// Atualizar os dados de um utilizador
 routes.patch('/users', async (req, res) => {
-    const gotUsers = Users.modifyUsers();
-    res.json(gotUsers);
+    const {oldUsername, newUsername, newEmail, newPassword, newPhone} = req.body;
+    const newUser = {username: newUsername, email: newEmail, password: newPassword, phone: newPhone}; 
+    const oldUser = Users.checkUser(oldUsername);
+    const updateUser = Users.updateUser(oldUser, newUser);
+    res.status(200).send(updateUser);
+    res.status(400).send(`Não foi possivel atualizar o utilizador com o username: ${username}.`);
 })
 
+// Apagar um utilizador através do username
 routes.delete('/users', async (req, res) => {
-    const gotUsers = Users.deleteUsers();
-    res.json(gotUsers);
+    const username = req.body;
+    const deletedUser = Users.deleteUser(username);
+    res.status(200).send(deletedUser);
+    res.status(400).send(`Não foi possivel apagar o utulizador com o username: ${username}.`);
+})
+
+// Apagar todos os utilizadores
+routes.delete('/users', async (req, res) => {
+    const deletedUsers = Users.deleteUsers();
+    res.status(200).send(deletedUsers);
+    res.status(400).send(`Não foi possivel apagar os utilizadores.`);
 })
 
 module.exports = routes;
--- Controllers/users.js
+++ Controllers/users.js
@@ -8,13 +8,42 @@ const SQL = require('../Controllers/sql');
 
 const UsersDB = SQL.useSchema(UsersSchema);*/
 
+
+// Procura todos os utilizadores na BD
 users.checkUsers = async function(){
-    const fetchedUsers = await UsersDB.find({});
-    return fetchedUsers;
+    const gotUsers = await UsersDB.find({});
+    return gotUsers;
 }
 
-users.updateUser = async function(oldValue, newValue){
-    const updateUser = await UsersDB.findOneAndUpdate(oldValue, newValue);
+// Procura um utilizador na BD pelo seu username
+users.checkUser = async function(username){
+    const gotUser = await UsersDB.find({username});
+    return gotUser;
+}
+
+// Atualiza um utilizador na BD pelos valores no body
+users.updateUser = async function(oldUser, newUser){
+    const updatedUser = await UsersDB.findOneAndUpdate(oldUser, newUser);
+    return updatedUser;
+}
+
+// Cria um utilizador na BD pelos valores no body
+users.createUser = async function(newUserData){
+    const newUser = UsersDB({username: newUserData.username, email: newUserData.email, password: newUserData.password, phone: newUserData.phone});
+    const createdUser = await newUser.save();
+    return createdUser;
+}
+
+// Apaga um utilizador da BD pelo seu username
+users.deleteUser = async function(username){
+    const deletedUser = await UsersDB.findOneAndDelete(username);
+    return deletedUser;
+}
+
+// Apaga todos os utilizadores da BD
+users.deleteUsers = async function(){
+    const deletedUsers = await UsersDB.deleteMany({});
+    return deletedUsers;
 }
 
 module.exports = users;
