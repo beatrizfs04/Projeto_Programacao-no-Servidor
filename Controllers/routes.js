61bec6215e82b7c3bd8e21b97d8fc28c828db67b
manu-pad <madilhaaz@gmail.com>
2024-05-21 17:01:26 +0100

    :)

--- Controllers/routes.js
+++ Controllers/routes.js
@@ -6,51 +6,84 @@ const Users = require('../Controllers/users');
 
 // Ver todos os utilizadores
 routes.get('/users', async (req, res) => {
-    const gotUsers = Users.checkUsers();
-    res.status(200).send(gotUsers);
-    res.status(400).send(`Não foi possivel aceder à informação da UsersDB.`);
+    try{
+        const gotUsers = Users.checkUsers();
+        res.status(200).send(gotUsers);        
+    } catch {
+        res.status(400).send(`Não foi possivel aceder à informação da UsersDB.`);        
+    }
 })
 
 // Ver um utilizador através do username
 routes.get('/users', async (req, res) => {
-    const username = req.body;
-    const gotUser = Users.checkUser(username);
-    res.status(200).send(gotUser);
-    res.status(400).send(`Não foi possivel encontrar o utilizador com o username: ${username}.`);
+    try {
+        const username = req.body;
+        const gotUser = Users.checkUser(username);
+        res.status(200).send(gotUser);
+    } catch {
+        res.status(400).send(`Não foi possivel encontrar o utilizador com o username: ${username}.`);        
+    }
 })
 
 // Criar um utilizador
 routes.post('/users', async (req, res) => {
-    const {username, email, password, phone} = req.body;
-    const newUser = {username: username, email: email, password: password, phone: phone};
-    const createdUser = Users.createUser(newUser);
-    res.status(200).send(createdUser);
-    res.status(400).send(`Não foi possivel acrescentar o utilizador com o username: ${username}, o email: ${email}, a password: ${password}, o phone: ${phone}.`);
+    try {
+        const {username, email, password, phone} = req.body;
+        const newUser = {username: username, email: email, password: password, phone: phone};
+        const createdUser = Users.createUser(newUser);
+        res.status(200).send(createdUser);
+    } catch {
+        res.status(400).send(`Não foi possivel acrescentar o utilizador com o username: ${username}, o email: ${email}, a password: ${password}, o phone: ${phone}.`);
+    }
 })
 
 // Atualizar os dados de um utilizador
 routes.patch('/users', async (req, res) => {
-    const {oldUsername, newUsername, newEmail, newPassword, newPhone} = req.body;
-    const newUser = {username: newUsername, email: newEmail, password: newPassword, phone: newPhone}; 
-    const oldUser = Users.checkUser(oldUsername);
-    const updateUser = Users.updateUser(oldUser, newUser);
-    res.status(200).send(updateUser);
-    res.status(400).send(`Não foi possivel atualizar o utilizador com o username: ${username}, para o novo user: ${newUser}.`);
+    try {
+        const {oldUsername, newUsername, newEmail, newPassword, newPhone} = req.body;
+        const newUser = {username: newUsername, email: newEmail, password: newPassword, phone: newPhone}; 
+        const oldUser = Users.checkUser(oldUsername);
+        const updateUser = Users.updateUser(oldUser, newUser);
+        res.status(200).send(updateUser);
+    } catch {
+        res.status(400).send(`Não foi possivel atualizar o utilizador com o username: ${username}, para o novo user: ${newUser}.`);
+    }
 })
 
 // Apagar um utilizador através do username
 routes.delete('/users', async (req, res) => {
-    const username = req.body;
-    const deletedUser = Users.deleteUser(username);
-    res.status(200).send(deletedUser);
-    res.status(400).send(`Não foi possivel apagar o utulizador com o username: ${username}.`);
+    try {
+        const username = req.body;
+        const deletedUser = Users.deleteUser(username);
+        res.status(200).send(deletedUser);
+    } catch {
+        res.status(400).send(`Não foi possivel apagar o utulizador com o username: ${username}.`);        
+    }
 })
 
 // Apagar todos os utilizadores
 routes.delete('/users', async (req, res) => {
-    const deletedUsers = Users.deleteUsers();
-    res.status(200).send(deletedUsers);
-    res.status(400).send(`Não foi possivel apagar os utilizadores.`);
+    try {
+        const deletedUsers = Users.deleteUsers();
+        res.status(200).send(deletedUsers);
+    } catch {
+        res.status(400).send(`Não foi possivel apagar os utilizadores.`);
+    }
+})
+
+// Login de Utilizadores
+routes.post('/login', async (req, res) => {
+    const {username , password} = req.body;
+    try {
+        const gotUser = Users.checkUser(username);
+        if (gotUser.password == password) {
+            res.status(200).send(gotUser);
+        } else {
+            res.status(400).send("Incorrect Password.");
+        }
+    } catch {
+        res.status(404).send("Username Not Found.");
+    }
 })
 
 module.exports = routes;
--- Controllers/users.js
+++ Controllers/users.js
@@ -2,11 +2,39 @@ const express = require('express');
 const users = express.Router();
 const SQL = require('../Controllers/sql');
 
-/* const UsersSchema = SQL.createSchema({
-    username: String,
+//Schema_Users
+const UsersSchema = SQL.createSchema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [8, 'Tamanho minimo é 8.'],
+        maxlength: [64, 'Tamanho máximo é 64.'],
+    },
+    email: {
+        type: String,
+        required: true,
+        maxlength: [255, 'Tamanho máximo é 255.'],
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} não é um endereço de e-mail válido!'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [8, 'Tamanho minimo é 8.'],
+        maxlength: [20, 'Tamanho máximo é 20.'],
+    },
+    phone: {
+        type: Number,
+        required: true,
+        minlength: [9, 'Tamanho minimo é 9.'],
+        maxlength: [9, 'Tamanho máximo é 9.'],
+    },
 })
 
-const UsersDB = SQL.useSchema(UsersSchema);*/
+const UsersDB = SQL.useSchema(UsersSchema);
 
 
 // Procura todos os utilizadores na BD
