6d0944cf72265a78165661573ff7e38b424a0419
oSLaYN <rodrigo.6640@gmail.com>
2024-05-24 14:04:29 +0100

    Update Drones Controller - Not Finished Yet

--- Controllers/drones.js
+++ Controllers/drones.js
@@ -15,4 +15,30 @@ drones.checkDrones = async function(){
     return gotDrones;
 }
 
+drones.checkDrone = async function(/* Conteudo a Procurar na Drone */){
+    const gotDrone = await DronesDB.find({/* Conteudo a Procurar na Drone */});
+    return gotDrone;
+}
+
+drones.updateDrone = async function(oldDrone, newDrone){
+    const updatedDrone = await DronesDB.findOneAndUpdate(oldDrone, newDrone);
+    return updatedDrone;
+}
+
+drones.createDrone = async function(newDroneData){
+    const newDrone = DronesDB(newDroneData);
+    const createdDrone = await newDrone.save();
+    return createdDrone;
+}
+
+drones.deletedDrone = async function(droneData){
+    const deletedDrone = await DronesDB.findOneAndDelete(droneData);
+    return deletedDrone;
+}
+
+drones.deleteDrones = async function(){
+    const deletedDrones = await DronesDB.deleteMany({});
+    return deletedDrones;
+}
+
 module.exports = drones;
