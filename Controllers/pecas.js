eec5b9169484ac70edad41d59ed563359495db87
PatalJunior <81658559+PatalJunior@users.noreply.github.com>
2024-05-28 01:29:43 +0100

    try catch error

--- Controllers/pecas.js
+++ Controllers/pecas.js
@@ -6,49 +6,73 @@ const validator = require('validator');
 //Schema_Pecas
 
 const PecasSchema = SQL.createSchema({
-    nomePeca: { 
-        type: String, 
+    nomePeca: {
+        type: String,
         required: true,
         maxlength: [25, 'Tamanho máximo é 25.']
     },
-    quantidade: { 
-        type: Number, 
-        required: true 
+    quantidade: {
+        type: Number,
+        required: true
     }
 }, "pecas")
 
 const PecasDB = SQL.useSchema(PecasSchema, "pecas");
 
 // Procura todos as peças na BD
-pecas.checkPecas = async function(){
-    const gotPecas = await PecasDB.find({});
-    return gotPecas;
+pecas.checkPecas = async function () {
+    try {
+        const gotPecas = await PecasDB.find({});
+        return gotPecas;
+    } catch (err) {
+        throw err;
+    }
 }
 
-pecas.checkPeca = async function(nomePeca){
-    const gotPeca = await PecasDB.find({nomePeca: nomePeca});
-    return gotPeca;
+pecas.checkPeca = async function (nomePeca) {
+    try {
+        const gotPeca = await PecasDB.findOne({ nomePeca: nomePeca });
+        return gotPeca;
+    } catch (err) {
+        throw err;
+    }
 }
 
-pecas.updatePeca = async function(oldPeca, newPeca){
-    const updatedPeca = await PecasDB.findOneAndUpdate(oldPeca, newPeca);
-    return updatedPeca;
+pecas.updatePeca = async function (oldPeca, newPeca) {
+    try {
+        const updatedPeca = await PecasDB.findOneAndUpdate(oldPeca, newPeca, { new: true }); // New: true para retornar a peça atualizada, caso contrário retorna a antiga
+        return updatedPeca;
+    } catch (err) {
+        throw err;
+    }
 }
 
-pecas.createPeca = async function(newPecaData){
-    const newPeca = PecasDB(newPecaData);
-    const createdPeca = await newPeca.save();
-    return createdPeca;
+pecas.createPeca = async function (newPecaData) {
+    try {
+        const newPeca = PecasDB(newPecaData);
+        const createdPeca = await newPeca.save();
+        return createdPeca;
+    } catch (err) {
+        throw err;
+    }
 }
 
-pecas.deletePeca = async function(nomePeca){
-    const deletedPeca = await PecasDB.findOneAndDelete(nomePeca);
-    return deletedPeca;
+pecas.deletePeca = async function (nomePeca) {
+    try {
+        const deletedPeca = await PecasDB.findOneAndDelete(nomePeca);
+        return deletedPeca;
+    } catch (err) {
+        throw err;
+    }
 }
 
-pecas.deletePecas = async function(){
-    const deletedPecas = await PecasDB.deleteMany({});
-    return deletedPecas;
+pecas.deletePecas = async function () {
+    try {
+        const deletedPecas = await PecasDB.deleteMany({});
+        return deletedPecas;
+    } catch (err) {
+        throw err;
+    }
 }
 
 module.exports = pecas;
